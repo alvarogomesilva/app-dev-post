7d7478f23c01a0cac9d448bbd7d4e94e0a08b5bc
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-04-24 13:42:46 -0300

    start: first commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAENUxLMAFbCumQLYdqY1zKNXqBga0N_uc",
+  authDomain: "dev-post-dc268.firebaseapp.com",
+  projectId: "dev-post-dc268",
+  storageBucket: "dev-post-dc268.appspot.com",
+  messagingSenderId: "681711462994",
+  appId: "1:681711462994:web:14af1ddc15d867600b0d8b"
+};
+
+const app = initializeApp(firebaseConfig);
