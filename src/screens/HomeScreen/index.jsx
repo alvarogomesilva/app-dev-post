458b97f413cbf48a3f3b1fcc72ba126a04b7beb2
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-04-25 12:14:52 -0300

    feat: buscando posts de usuario

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,12 +1,18 @@
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { Header } from "../../components/Header";
-import { useState } from "react";
+import { useCallback, useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { query, orderBy, limit, collection, getDocs } from "firebase/firestore";
+import { firestore } from "../../firebase/firebaseConfig";
 
 export default function HomeScreen() {
+    const { user } = useContext(AuthContext)
     const navigation = useNavigation()
+    const [loading, setLoading] = useState(true)
+
     const [posts, setPosts] = useState([
         { id: '1', content: 'TEstes' },
         { id: '2', content: 'TEstes' },
@@ -14,19 +20,56 @@ export default function HomeScreen() {
         { id: '4', content: 'TEstes' }
     ])
 
+    useFocusEffect(
+        useCallback(() => {
+            let isActive = true
+
+            async function fetchPosts() {
+                try {
+                    const docRef = collection(firestore, "posts")
+                    const q = query(docRef, orderBy("created", "desc"), limit(5));
+                    const querySnapshot = await getDocs(q);
+
+                    if (isActive) {
+                        setPosts([])
+                        const postList = []
+                        querySnapshot.docs.map((u) => {
+                            postList.push({ ...u.data(), id: u.id })
+                        })
+                        setPosts(postList)
+                        setLoading(false)
+                    }
+
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+
+            fetchPosts()
+            return () => isActive = false
+        }, [])
+    )
+
     return (
         <View style={styles.container}>
             <Header />
 
-            <FlatList 
-                style={styles.flatlist}
-                data={posts}
-                renderItem={({ item }) => (<Text>Teste</Text>)}
-            
-            />
+            {loading ? (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size="large" color="#E52246" />
 
-            <TouchableOpacity 
-                style={styles.button} 
+                </View>
+            ) : (
+                <FlatList
+                    style={styles.flatlist}
+                    data={posts}
+                    renderItem={({ item }) => (<Text>Teste</Text>)}
+
+                />
+            )}
+
+            <TouchableOpacity
+                style={styles.button}
                 activeOpacity={0.8}
                 onPress={() => navigation.navigate('NewPostScreen')}
             >
